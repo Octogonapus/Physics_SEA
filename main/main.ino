7cfb16a64a58b347bc9cbcee5d3b681a8fbf1414
Octogonapus <firey45@gmail.com>
2016-04-26 20:02:39 -0400

    Added force calculation

--- main/main.ino
+++ main/main.ino
@@ -1,17 +1,38 @@
 //Motor driver pins
-const int EN1 = 10, IN_A1 = 8, IN_B1 = 9, PWM1 = 3;
+const int EN1 = 8, IN_A1 = 7, IN_B1 = 4, PWM1 = 3;
 
-//Input pins
+//Digital input pins
 const int killSwitchButton = 2;
 
+//Analog input pins
+const int slidePot = A0;
+
+//Spring constant
+const float springK = 26.30;
+
+//Current load
+float load = 0;
+
 void setup() {
+  Serial.begin(9600);
+  
+  //Motor driver pins
   pinMode(IN_A1, OUTPUT);
   pinMode(IN_B1, OUTPUT);
   pinMode(EN1, OUTPUT);
   pinMode(PWM1, OUTPUT);
+
+  //Digital input pins
+  pinMode(killSwitchButton, INPUT);
 }
 
 void loop() {
+  //Update current load
+  load = springK * ((((analogRead(slidePot) / 1023.0) * 5.0) / 2.5) - 1);
+
+  //Write current load to serial output
+  Serial.println(load);
+  
   //Kill switch
   if (digitalRead(killSwitchButton) == 1)
   {
